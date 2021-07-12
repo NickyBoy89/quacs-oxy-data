177ece079a15639c5065bd5e63f4e0ee807c6f25
Nicholas Novak <nicholasmnovak@gmail.com>
2021-07-11 19:18:46 -0700

    Parallelized generation script

--- genSemesters.sh
+++ genSemesters.sh
@@ -10,31 +10,36 @@ else
   python3 quacs-oxy-scraper/semesters/main.py
 fi
 
+# Function that scrapes a semester
+function scrape_semester () {
+  # RateMyProfessors scraper is not really used for anything
+  # python3 quacs-oxy-scraper/rmp_scraper/main.py $semester
+
+  # Generate the catalog.json because the sis_scraper needs it
+  python3 quacs-oxy-scraper/catalog_scraper/main.py $1
+  # Launch all the other scrapers in their own processes
+  python3 quacs-oxy-scraper/prerequisite_scraper/main.py $1 && mv prerequisites.json semester_data/$1
+  python3 quacs-oxy-scraper/school_scraper/main.py $1 && mv schools.json semester_data/$1
+  python3 quacs-oxy-scraper/sis_scraper/main.py $1 && mv courses.json mod.rs semester_data/$1
+  # Wait for them all to complete
+  # Move the catalog to the correct place, since the sis scraper has finished using it
+  mv catalog.json semester_data/$1
+}
+
 # Loop through the semesters and scrape them
 while read semester; do
   # Create a folder for each semester
   mkdir -p semester_data/$semester
   echo "Starting work on semester $semester"
   # To skip a range of semesters that are already generated, set this number to the highest semester generated
+  # 0 just disables it
   if [ $semester -lt 0 ]
   then
     echo "Skipping semester $semester"
     continue
   fi
 
-  # RateMyProfessors scraper is not really used for anything
-  # python3 quacs-oxy-scraper/rmp_scraper/main.py $semester
-
-  # Generate the catalog.json because the sis_scraper needs it
-  python3 quacs-oxy-scraper/catalog_scraper/main.py $semester
-  # Launch all the other scrapers in their own processes
-  python3 quacs-oxy-scraper/prerequisite_scraper/main.py $semester && mv prerequisites.json semester_data/$semester &
-  python3 quacs-oxy-scraper/school_scraper/main.py $semester && mv schools.json semester_data/$semester &
-  python3 quacs-oxy-scraper/sis_scraper/main.py $semester && mv courses.json mod.rs semester_data/$semester &
-  # Wait for them all to complete
-  wait
-  # Move the catalog to the correct place, since the sis scraper has finished using it
-  mv catalog.json semester_data/$semester
+  scrape_semester $semester &
 done <semesters.json
 
 echo "Create meta.json"

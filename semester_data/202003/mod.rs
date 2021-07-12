09113dbf41eca81b9975fae82b3ada861390a05a
Nicholas Novak <nicholasmnovak@gmail.com>
2021-07-12 10:25:27 -0700

    Re-scraped correct data

--- semester_data/202003/mod.rs
+++ semester_data/202003/mod.rs
@@ -4,8 +4,10 @@
             pub const BIT_VEC_LEN: usize = 0;
 
             pub static CRN_TIMES: Map<u32, [u64; BIT_VEC_LEN]> = phf_map! {
-            
+            	3115u32 => [],
+
             };
 
             pub static CRN_COURSES: Map<u32, &'static str> = phf_map! {
-            };
+            	3115u32 => "CORE-100",
+};

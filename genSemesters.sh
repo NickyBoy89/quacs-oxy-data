20c0751edb663306e962b5fbd470c749878a2ace
Nicholas Novak <nicholasmnovak@gmail.com>
2021-07-12 00:42:11 -0700

    Parallelized generation script and re-generated semesters

--- genSemesters.sh
+++ genSemesters.sh
@@ -12,18 +12,19 @@ fi
 
 # Function that scrapes a semester
 function scrape_semester () {
+  cd semester_data/$1
   # RateMyProfessors scraper is not really used for anything
   # python3 quacs-oxy-scraper/rmp_scraper/main.py $semester
 
   # Generate the catalog.json because the sis_scraper needs it
-  python3 quacs-oxy-scraper/catalog_scraper/main.py $1
+  python3 ../../quacs-oxy-scraper/catalog_scraper/main.py $1
   # Launch all the other scrapers in their own processes
-  python3 quacs-oxy-scraper/prerequisite_scraper/main.py $1 && mv prerequisites.json semester_data/$1
-  python3 quacs-oxy-scraper/school_scraper/main.py $1 && mv schools.json semester_data/$1
-  python3 quacs-oxy-scraper/sis_scraper/main.py $1 && mv courses.json mod.rs semester_data/$1
-  # Wait for them all to complete
-  # Move the catalog to the correct place, since the sis scraper has finished using it
-  mv catalog.json semester_data/$1
+  python3 ../../quacs-oxy-scraper/prerequisite_scraper/main.py $1 &
+  python3 ../../quacs-oxy-scraper/school_scraper/main.py $1 &
+  # The sis scraper needs the catalog
+  python3 ../../quacs-oxy-scraper/sis_scraper/main.py $1
+  # Wait for all the scrapers to finish
+  wait
 }
 
 # Loop through the semesters and scrape them
@@ -42,8 +43,14 @@ while read semester; do
   scrape_semester $semester &
 done <semesters.json
 
+# Wait for semesters to finish scraping
+wait
+
 echo "Create meta.json"
-echo {\"last_updated\":\"$(date --iso-8601=seconds -u)\"} > data/meta.json
+echo {\"last_updated\":\"$(date --iso-8601=seconds -u)\"} > meta.json
 
 echo "Generate the prerequisite graph"
 python3 quacs-oxy-scraper/prerequisites_graph/main.py semester_data prereq_graph.json
+
+echo "Clean up files"
+rm semesters.json
